df9a5f2e45f35a84ec6d78cc02cd7f0cbf5d9ac0
ziyang.liu <ziyang.liu@mobilemd.cn>
2023-08-03 20:06:51 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+  return (
+    <div className='w-full h-full flex justify-center'>
+      <div className='flex flex-row w'>
+        <div>Left</div>
+        <div>Main</div>
+        <div>Right</div>
+      </div>
+    </div>
+  )
+}
